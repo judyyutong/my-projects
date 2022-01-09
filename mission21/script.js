1c498f8d1962591f82da1ecad00375fbc392a318
Judy <judyyutong@gmail.com>
2022-01-10 00:52:50 +0800

    set html, css, js files, structuring HTML and adding CSS to make a pretty  seat-selecting page.

--- mission21/script.js
+++ mission21/script.js
@@ -24,35 +24,27 @@ function isValidEmail(email){
     return re.test(String(email).toLowerCase());
 }
 
+// Check Required Field
+function checkRequired(inputArr) {
+   inputArr.forEach(function(input){
+       if(input.value.trim() === ''){
+           showError(input, `${getFieldName(input)} is required`);
+       }else{
+           showSuccess(input)
+       }
+   }); 
+}
+
+// Get Field Name
+function getFieldName(input){
+     return input.id.charAt(0).toUpperCase();
+}
+
+
 // Event Listener
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-  
-    if (username.value === ''){
-        showError(username, 'Username is required.');
-    } else{
-        showSuccess(username);
-    }
-
-    if (email.value === ''){
-        showError(email, 'Email is required.');
-    } else if(!isValidEmail(email.value)){
-        showError(email, 'Email is not valid.')
-    } else{ 
-        showSuccess(email);
-    }
-
-    if (password.value === ''){
-        showError(password, 'Password is required.');
-    } else{
-        showSuccess(password);
-    }
-
-    if (password2.value === ''){
-        showError(password2, 'Password2 is required.');
-    } else{
-        showSuccess(password2);
-    }
+    checkRequired([username, email, password, password2]); 
 
 });
 
